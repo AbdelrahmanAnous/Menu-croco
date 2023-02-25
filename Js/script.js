2a5ade927e0f4da3558a52b34bffd79d5e26b006
Abdelrahman Anous <116388640+AbdelrahmanAnous@users.noreply.github.com>
2023-02-25 23:58:29 +0200

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        375: {
+            items: 3
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
